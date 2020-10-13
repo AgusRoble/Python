553b5056563c104bccd2f96608dab34d795405b9
LucasN <72484790+lucasN139@users.noreply.github.com>
2020-10-13 18:34:03 -0300

    Agregado de Apellido y sistema de igualdad
    xd

--- Comparacion Edades.py
+++ Comparacion Edades.py
@@ -1,18 +1,25 @@
 
 print("Ingrese un nombre: ")
 Nombre1 = input()
+print("Ingrese un apellido: ")
+Apellido1 = input()
 print("Ingrese su edad: ")
 Edad1 = input()
 print("Ingrese otro nombre: ")
 Nombre2 = input()
+print("Ingrese un apellido: ")
+Apellido2 = input()
 print("Ingrese su edad: ")
 Edad2 = input()
 
-if Edad1 < Edad2:
-	print(f"{Nombre1} es menor que {Nombre2}")
-elif Edad1 > Edad2:
-	print(f"{Nombre1} es mayor que {Nombre2}")
-elif Edad1 == Edad2:
-	print(f" {Nombre1} y {Nombre2} tienen la misma edad")
+if Nombre1 == Nombre2 or Apellido1 == Apellido2:
+	print("no pueden haber mismos nombres y apellidos")
 else:
-	print("Datos no validos")
+	if Edad1 < Edad2:
+		print(f"{Nombre1} es menor que {Nombre2}")
+	elif Edad1 > Edad2:
+		print(f"{Nombre1} es mayor que {Nombre2}")
+	elif Edad1 == Edad2:
+		print(f" {Nombre1} y {Nombre2} tienen la misma edad")
+	else:
+		print("Datos no validos")
